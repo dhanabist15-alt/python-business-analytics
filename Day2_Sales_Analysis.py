938ce6ee812dd06d1c06eef053b4933ae45d77dc
dhanabist15-alt <dhanabist15@gmail.com>
2026-01-30 15:11:19 +0545

    Day 2 : Basics Sales data analysis using Python

---
+++ Day2_Sales_Analysis.py
@@ -0,0 +1,15 @@
+# Day 2: Sales Data Analysis
+
+# Project Sales Data
+products = ["Laptop", "Mobile", "Tablet", "Headphone"]
+sales = [ 5, 10, 13, 7]
+price = [800, 600, 500, 100]
+
+total_revanue = 0
+print("Sales Report\n")
+for i in range(len(oroducts)):
+  revanue = sales[i] * price[i]
+  total_revanue += revanue
+  print(product[i], "- Revanue:",revanue)
+print("\nTotal Revanue:", total_revanue)
+  
